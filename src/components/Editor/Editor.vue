ef9d84af2abd0b143a3958760cb29f353a15669e
Ed <me@eddieabbondanz.io>
2021-03-18 07:25:49 -0400

    Mocked up buttons on editor toolbar

--- src/components/Editor/Editor.vue
+++ src/components/Editor/Editor.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="has-background-light is-flex-grow-1 has-text-dark">
         <editor-toolbar />
-        <p class="has-text-dark">Editor</p>
-        <i class="fab fa-medium"></i>
-        <i class="far fa-envelope"></i>
+        <p class="has-text-dark">Editor Window</p>
     </div>
 </template>
 
--- src/components/Editor/EditorToolbar.vue
+++ src/components/Editor/EditorToolbar.vue
@@ -1,9 +1,35 @@
 <template>
-    <div class="has-background-light has-border-bottom-1-dark p-1">
-        TOOLBAR
-        <span class="icon is-medium">
-            <i class="mdi mdi-bell has-text-dark"></i>
-        </span>
+    <div class="has-background-light has-border-bottom-1-dark p-1 is-flex is-align-center">
+        <div class="buttons has-addons mb-0 mx-1">
+            <button class="button mb-0" style="height: 30px" title="Edit">
+                <span class="icon is-small">
+                    <i class="fas fa-pen"></i>
+                </span>
+            </button>
+            <button class="button mb-0" style="height: 30px" title="Delete">
+                <span class="icon is-small">
+                    <i class="fas fa-trash"></i>
+                </span>
+            </button>
+        </div>
+
+        <div class="buttons has-addons mb-0 mx-1">
+            <button class="button mb-0" title="Edit notebook links" style="height: 30px">
+                <span class="icon is-small">
+                    <i class="fas fa-book"></i>
+                </span>
+            </button>
+            <button class="button mb-0" title="Edit tags" style="height: 30px">
+                <span class="icon is-small">
+                    <i class="fas fa-tag"></i>
+                </span>
+            </button>
+            <button class="button mb-0" title="Add to favorites" style="height: 30px">
+                <span class="icon is-small">
+                    <i class="fas fa-star"></i>
+                </span>
+            </button>
+        </div>
     </div>
 </template>
 
