496b24ba563497cbc8ecf30991cf062d31f815ba
Ed <me@eddieabbondanz.io>
2021-04-12 19:29:38 -0400

    Cleaned up app mutations by removing SET_STATE

--- src/store/modules/app/mutations.ts
+++ src/store/modules/app/mutations.ts
@@ -4,23 +4,6 @@ import { Notebook } from '../notebooks/state';
 import { AppState } from './state';
 
 export const mutations: MutationTree<AppState> = {
-    // OLD
-    UPDATE_STATE: (state, kv: { key: string; value: any }) => {
-        // i = 'a.b.c' -> a['a']['b']['c'] = v
-        const recurse = (a: any, i: string, v: any) => {
-            if (i.indexOf('.') < 0) {
-                a[i] = v;
-                return;
-            }
-
-            const split = i.split('.');
-            const localI = split.shift()!;
-            recurse(a[localI], split.join('.'), v);
-        };
-
-        recurse(state, kv.key, kv.value);
-    },
-
     SET_CURSOR_TITLE(state, title: string) {
         state.cursor.title = title;
     },
