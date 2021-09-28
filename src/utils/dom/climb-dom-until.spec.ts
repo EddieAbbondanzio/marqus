bad62066e02992fdffdc06102a5694509df013f0
Ed Abbondanzio <me@eddieabbondanz.io>
2021-09-28 09:14:47 -0400

    Cleaned up climbDomUntil util to have a cleaner type signature

--- src/utils/dom/climb-dom-until.spec.ts
+++ src/utils/dom/climb-dom-until.spec.ts
@@ -1,38 +1,27 @@
-import { climbDomUntil } from "@/utils/dom/climb-dom-hierarchy";
+import { climbDomUntil } from "./climb-dom-until";
 
 describe("climbDomUntil()", () => {
-  let parent = document.createElement("div");
-  let child = document.createElement("div");
+  const parent = document.createElement("div");
+  const child = document.createElement("div");
   parent.append(child);
 
   it("it checks for stop condition before doing anything", () => {
-    const r = climbDomUntil<boolean>(child, {
-      match: el => true,
-      stop: el => true
-    });
-
+    const r = climbDomUntil(child, () => true, { stop: () => true });
     expect(r).toBeFalsy();
   });
 
   it("returns true when match on initial element", () => {
-    const r = climbDomUntil<boolean>(child, {
-      match: el => el == child
-    });
-
+    const r = climbDomUntil(child, el => el === child);
     expect(r).toBeTruthy();
   });
 
   it("returns true when match on parent", () => {
-    const r = climbDomUntil<boolean>(child, {
-      match: el => el == parent
-    });
-
+    const r = climbDomUntil(child, el => el === parent);
     expect(r).toBeTruthy();
   });
 
   it("returns match value on match and passed", () => {
-    const r = climbDomUntil(child, {
-      match: el => el == parent,
+    const r = climbDomUntil(child, el => el === parent, {
       matchValue: () => 3
     });
 
@@ -40,27 +29,23 @@ describe("climbDomUntil()", () => {
   });
 
   it("returns false when no match, and no default value", () => {
-    const r = climbDomUntil<boolean>(child, {
-      match: el => false
-    });
+    const r = climbDomUntil(child, () => false);
 
     expect(r).toBeFalsy();
   });
 
   it("returns default value on stop condition if passed", () => {
-    const r = climbDomUntil(child, {
+    const r = climbDomUntil(child, () => true, {
       stop: () => true,
-      match: el => true,
-      matchValue: () => 42,
-      defaultValue: () => 3
+      matchValue: 42,
+      defaultValue: 3
     });
 
     expect(r).toBe(3);
   });
 
   it("returns default value on failed if passed", () => {
-    const r = climbDomUntil(child, {
-      match: el => false,
+    const r = climbDomUntil(child, () => false, {
       matchValue: () => 42,
       defaultValue: () => 3
     });
