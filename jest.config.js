246aa909e557840862be48723771c0eba64b839f
Ed Abbondanzio <me@eddieabbondanz.io>
2021-10-28 07:03:52 -0400

    Removed old cached files

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-  transform: {
-    "^.+\\.vue$": "vue-jest",
-  },
-  testMatch: ["./src/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-};
--- serve.sh
+++
@@ -1,6 +0,0 @@
-# Switch us to Node.js version 14.10.0 (Won't be needed when we switch laptops)
-. ~/.nvm/nvm.sh
-nvm use 14.10.0
-
-# Start the app
-vue-cli-service electron:serve
