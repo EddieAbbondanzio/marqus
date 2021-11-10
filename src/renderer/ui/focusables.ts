8e16a9877e53a93c7915eb1026434556e9e3ff05
Ed Abbondanzio <me@eddieabbondanz.io>
2021-11-10 08:00:50 -0500

    Track user focus via focusables

--- src/renderer/ui/focusables.ts
+++ src/renderer/ui/focusables.ts
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { RequireOne } from "../../shared/utils/types";
 import { findParent } from "./findParent";
 
 export const FOCUSABLE_ID_ATTRIBUTE = "data-focusable-id";
@@ -13,6 +14,9 @@ let focusables: Focusable[] = [];
 let active: Focusable | undefined;
 let enabled: boolean = false;
 
+/**
+ * Enable tracking user focus. Should be called in the root component.
+ */
 export function useFocusables() {
   useEffect(() => {
     if (enabled) {
@@ -29,6 +33,10 @@ export function useFocusables() {
   });
 }
 
+/**
+ * Register a focusable that should be tracked.
+ * @param focusable Focusable element to register.
+ */
 export function registerFocusable(focusable: Focusable) {
   const existing = focusables.find((f) => f.el === focusable.el);
 
@@ -40,6 +48,10 @@ export function registerFocusable(focusable: Focusable) {
   focusables.push(focusable);
 }
 
+/**
+ * Stop tracking a focusable element.
+ * @param focusable Focusable to no longer track.
+ */
 export function removeFocusable(focusable: Focusable) {
   const index = focusables.findIndex((f) => f.el === focusable.el);
 
@@ -48,21 +60,60 @@ export function removeFocusable(focusable: Focusable) {
   }
 }
 
-export function isFocused(focusable: Focusable | string) {
-  const f =
-    typeof focusable === "string"
-      ? focusables.find((f) => f.name === focusable)
-      : focusable;
+/**
+ * Check to see if a focusable element currently has user focus.
+ * @param opts id or name of the focusable.
+ * @param checkChildren If we should consider a
+ * focusable focused when a child focusable is focused.
+ * @returns True if the active focusable matches.
+ */
+export function isFocused(
+  opts: RequireOne<Focusable, "id" | "name">,
+  checkChildren: boolean = false
+) {
+  const focusable = findFocusable(opts);
 
-  if (f == null) {
-    throw Error(`No focusable found`);
-  }
-
-  if (f === active) {
+  if (focusable === active) {
     return true;
+  } else if (checkChildren) {
+    throw Error("Not implemented.");
   } else {
     return false;
   }
+}
+
+/**
+ * Focus user input into a focusable element.
+ * @param opts id or name of the focusable.
+ */
+export function focus(opts: RequireOne<Focusable, "id" | "name">) {
+  const focusable = findFocusable(opts);
+  focusable.el.focus();
+}
+
+/**
+ * Find a focusable by id or name. Throws if none found.
+ * @param opts id or name of the focusable.
+ * @returns The matching focusable.
+ */
+function findFocusable(opts: RequireOne<Focusable, "id" | "name">): Focusable {
+  let focusable: Focusable | undefined;
+
+  if (opts.id != null) {
+    focusable = focusables.find((f) => opts.id === f.id);
+
+    if (focusables == null) {
+      throw Error(`No focusable with id ${opts.id} found.`);
+    }
+  } else {
+    focusable = focusables.find((f) => opts.name === f.name);
+
+    if (focusables == null) {
+      throw Error(`No focusable with name ${opts.name} found.`);
+    }
+  }
+
+  return focusable!;
 }
 
 /**
