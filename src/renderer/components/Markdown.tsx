ed72936740e3bb7be95ecdee6d7ac97ddd260e5e
Ed Abbondanzio <me@eddieabbondanz.io>
2022-04-24 18:49:29 -0400

    Github flavored markdown

--- src/renderer/components/Markdown.tsx
+++ src/renderer/components/Markdown.tsx
@@ -2,7 +2,11 @@ import DOMPurify from "dompurify";
 import { marked } from "marked";
 import React from "react";
 
-export function Markdown(props: { content: string }) {
+marked.setOptions({
+  gfm: true,
+});
+
+export function Markdown(props: { content: string }): JSX.Element {
   const rendered = marked.parse(props.content ?? "");
   const clean = DOMPurify.sanitize(rendered);
   return (
