daf55ba5d818d4f143eab376c704cecb4c96ee04
Ed Abbondanzio <me@eddieabbondanz.io>
2022-04-29 23:25:47 -0400

    Utilie new context menus

--- src/renderer/menus/contextMenu.ts
+++ src/renderer/menus/contextMenu.ts
@@ -1,4 +1,7 @@
 import { useEffect, useMemo } from "react";
+import { Menu } from "../../shared/domain/ui";
+import { isDevelopment } from "../../shared/env";
+import { getSidebarMenuAttribute } from "../components/SidebarMenu";
 import { getShortcutLabels } from "../io/shortcuts";
 import { Store } from "../store";
 
@@ -12,32 +15,65 @@ export function useContextMenu(store: Store): void {
   const selected = store.state.ui.sidebar.selected?.[0];
 
   useEffect(() => {
-    const showMenu = () => {
-      void window.ipc("app.showContextMenu", [
-        {
-          label: "&RAVIOLI",
-          children: [
+    const showMenu = (ev: MouseEvent) => {
+      const target = getSidebarMenuAttribute(ev.target as HTMLElement);
+      const items: Menu[] = [];
+      if (focused === "sidebar") {
+        items.push({
+          label: "New note",
+          event: "sidebar.createNote",
+          eventInput: target,
+        });
+
+        if (target != null) {
+          items.push(
             {
-              label: "DO IT!",
-              shortcut: shortcutLabels["app.reload"],
-              event: "app.reload",
-              disabled: selected == null,
+              label: "Rename note",
+              event: "sidebar.renameNote",
+              eventInput: target,
+              shortcut: shortcutLabels["sidebar.renameNote"],
             },
-          ],
-        },
-        {
-          label: "&HIDDEN",
-          hidden: true,
-          event: "app.reload",
-        },
-      ]);
+            {
+              label: "Permanently delete",
+              event: "sidebar.deleteNote",
+              eventInput: target,
+              shortcut: shortcutLabels["sidebar.deleteNote"],
+            },
+            {
+              label: "Move to trash",
+              event: "sidebar.moveNoteToTrash",
+              eventInput: target,
+              shortcut: shortcutLabels["sidebar.moveNoteToTrash"],
+            }
+          );
+        }
+      }
+
+      if (isDevelopment()) {
+        items.push({
+          type: "separator",
+        });
+        items.push({
+          label: "Inspect element",
+          event: "app.inspectElement",
+          eventInput: target,
+          shortcut: shortcutLabels["app.inspectElement"],
+        });
+        items.push({
+          label: "Open dev tools",
+          event: "app.openDevTools",
+          shortcut: shortcutLabels["app.openDevTools"],
+        });
+      }
+
+      void window.ipc("app.showContextMenu", items);
     };
 
     window.addEventListener("contextmenu", showMenu);
     return () => {
       window.removeEventListener("contextmenu", showMenu);
     };
-  }, [focused, selected, shortcutLabels]);
+  }, [selected, focused, shortcutLabels]);
 
   useEffect(() => {
     const onClick = (ev: CustomEvent) => {
