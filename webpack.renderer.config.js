cde2e3d10964c2e557f3d4b13fd0c8fa141645fd
Ed Abbondanzio <me@eddieabbondanz.io>
2022-05-28 21:47:59 -0400

    Clean up webpack files

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 const rules = require("./webpack.rules");
-const plugins = require("./webpack.plugins");
 
 module.exports = {
   devServer: {
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     rules,
   },
-  plugins: [...plugins, new MonacoWebpackPlugin()],
+  plugins: [new ForkTsCheckerWebpackPlugin(), new MonacoWebpackPlugin()],
   resolve: {
     extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".scss", ".sass"],
   },
