94c75bf0bc2670325871b9fa475fcf3613cf061f
Ed Abbondanzio <me@eddieabbondanz.io>
2022-11-15 08:53:49 -0500

    Refactor out remaining describe uses

--- test/renderer/utils/findParent.spec.ts
+++ test/renderer/utils/findParent.spec.ts
@@ -1,55 +1,77 @@
 import { findParent } from "../../../src/renderer/utils/findParent";
 
-describe("climbDomUntil()", () => {
+test("findParent it checks for stop condition before doing anything", () => {
   const parent = document.createElement("div");
   const child = document.createElement("div");
   parent.append(child);
 
-  test("it checks for stop condition before doing anything", () => {
-    const r = findParent(child, () => true, { stop: () => true });
-    expect(r).toBeFalsy();
+  const r = findParent(child, () => true, { stop: () => true });
+  expect(r).toBeFalsy();
+});
+
+test("findParent returns true when match on initial element", () => {
+  const parent = document.createElement("div");
+  const child = document.createElement("div");
+  parent.append(child);
+
+  const r = findParent(child, el => el === child);
+  expect(r).toBeTruthy();
+});
+
+test("findParent returns true when match on parent", () => {
+  const parent = document.createElement("div");
+  const child = document.createElement("div");
+  parent.append(child);
+
+  const r = findParent(child, el => el === parent);
+  expect(r).toBeTruthy();
+});
+
+test("findParent returns match value on match and passed", () => {
+  const parent = document.createElement("div");
+  const child = document.createElement("div");
+  parent.append(child);
+
+  const r = findParent(child, el => el === parent, { 
+    matchValue: () => 3,
   });
 
-  test("returns true when match on initial element", () => {
-    const r = findParent(child, (el) => el === child);
-    expect(r).toBeTruthy();
+  expect(r).toBe(3);
+});
+
+test("findParent returns false when no match, and no default value", () => {
+  const parent = document.createElement("div");
+  const child = document.createElement("div");
+  parent.append(child);
+
+  const r = findParent(child, () => false);
+
+  expect(r).toBeFalsy();
+});
+
+test("findParent returns default value on stop condition if passed", () => {
+  const parent = document.createElement("div");
+  const child = document.createElement("div");
+  parent.append(child);
+
+  const r = findParent(child, () => true, {
+    stop: () => true,
+    matchValue: 42,
+    defaultValue: 3,
   });
 
-  test("returns true when match on parent", () => {
-    const r = findParent(child, (el) => el === parent);
-    expect(r).toBeTruthy();
+  expect(r).toBe(3);
+});
+
+test("findParent returns default value on failed if passed", () => {
+  const parent = document.createElement("div");
+  const child = document.createElement("div");
+  parent.append(child);
+
+  const r = findParent(child, () => false, {
+    matchValue: () => 42,
+    defaultValue: () => 3,
   });
 
-  test("returns match value on match and passed", () => {
-    const r = findParent(child, (el) => el === parent, {
-      matchValue: () => 3,
-    });
-
-    expect(r).toBe(3);
-  });
-
-  test("returns false when no match, and no default value", () => {
-    const r = findParent(child, () => false);
-
-    expect(r).toBeFalsy();
-  });
-
-  test("returns default value on stop condition if passed", () => {
-    const r = findParent(child, () => true, {
-      stop: () => true,
-      matchValue: 42,
-      defaultValue: 3,
-    });
-
-    expect(r).toBe(3);
-  });
-
-  test("returns default value on failed if passed", () => {
-    const r = findParent(child, () => false, {
-      matchValue: () => 42,
-      defaultValue: () => 3,
-    });
-
-    expect(r).toBe(3);
-  });
+  expect(r).toBe(3);
 });
--- test/shared/io/keyCode.spec.ts
+++ test/shared/io/keyCode.spec.ts
@@ -13,278 +13,256 @@ import {
   sortKeyCodes,
 } from "../../../src/shared/io/keyCode";
 
-describe("parseKey()", () => {
-  test.each([
-    ["Space", KeyCode.Space],
-    ["Escape", KeyCode.Escape],
-    ["Insert", KeyCode.Insert],
-    ["Delete", KeyCode.Delete],
-    ["Backquote", KeyCode.Backquote],
-    ["Minus", KeyCode.Minus],
-    ["Equal", KeyCode.Equal],
-    ["Backspace", KeyCode.Backspace],
-    ["Tab", KeyCode.Tab],
-    ["BracketLeft", KeyCode.BracketLeft],
-    ["BracketRight", KeyCode.BracketRight],
-    ["Backslash", KeyCode.Backslash],
-    ["CapsLock", KeyCode.CapsLock],
-    ["Quote", KeyCode.Quote],
-    ["Enter", KeyCode.Enter],
-    ["ShiftLeft", KeyCode.Shift],
-    ["ShiftRight", KeyCode.Shift],
-    ["Comma", KeyCode.Comma],
-    ["Period", KeyCode.Period],
-    ["Slash", KeyCode.Slash],
-    ["ControlLeft", KeyCode.Control],
-    ["ControlRight", KeyCode.Control],
-    ["AltLeft", KeyCode.Alt],
-    ["AltRight", KeyCode.Alt],
-    ["ArrowUp", KeyCode.ArrowUp],
-    ["ArrowDown", KeyCode.ArrowDown],
-    ["ArrowLeft", KeyCode.ArrowLeft],
-    ["ArrowRight", KeyCode.ArrowRight],
-    ["NumpadAdd", KeyCode.NumpadAdd],
-    ["NumpadSubtract", KeyCode.NumpadSubtract],
-    ["NumpadMultiply", KeyCode.NumpadMultiply],
-    ["NumpadDivide", KeyCode.NumpadDivide],
-    ["NumpadSeparator", KeyCode.NumpadSeparator],
-    ["NumpadDecimal", KeyCode.NumpadDecimal],
-    ["PageUp", KeyCode.PageUp],
-    ["PageDown", KeyCode.PageDown],
-  ]);
-
-  test("f keys", () => {
-    for (let i = 1; i <= 12; i++) {
-      expect(parseKeyCode(`F${i}`)).toBe(`f${i}`);
-    }
-  });
-
-  test("digits", () => {
-    for (let i = 0; i <= 9; i++) {
-      expect(parseKeyCode(`Digit${i}`)).toBe(i.toString());
-    }
-  });
-
-  test("alphabet letters", () => {
-    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-
-    for (const letter of alphabet) {
-      expect(parseKeyCode(`Key${letter}`)).toBe(letter.toLowerCase());
-    }
-  });
-
-  test("numpad digits", () => {
-    for (let i = 0; i <= 9; i++) {
-      expect(parseKeyCode(`Numpad${i}`)).toBe(`numpad${i}`);
-    }
-  });
+test.each([
+  ["Space", KeyCode.Space],
+  ["Escape", KeyCode.Escape],
+  ["Insert", KeyCode.Insert],
+  ["Delete", KeyCode.Delete],
+  ["Backquote", KeyCode.Backquote],
+  ["Minus", KeyCode.Minus],
+  ["Equal", KeyCode.Equal],
+  ["Backspace", KeyCode.Backspace],
+  ["Tab", KeyCode.Tab],
+  ["BracketLeft", KeyCode.BracketLeft],
+  ["BracketRight", KeyCode.BracketRight],
+  ["Backslash", KeyCode.Backslash],
+  ["CapsLock", KeyCode.CapsLock],
+  ["Quote", KeyCode.Quote],
+  ["Enter", KeyCode.Enter],
+  ["ShiftLeft", KeyCode.Shift],
+  ["ShiftRight", KeyCode.Shift],
+  ["Comma", KeyCode.Comma],
+  ["Period", KeyCode.Period],
+  ["Slash", KeyCode.Slash],
+  ["ControlLeft", KeyCode.Control],
+  ["ControlRight", KeyCode.Control],
+  ["AltLeft", KeyCode.Alt],
+  ["AltRight", KeyCode.Alt],
+  ["ArrowUp", KeyCode.ArrowUp],
+  ["ArrowDown", KeyCode.ArrowDown],
+  ["ArrowLeft", KeyCode.ArrowLeft],
+  ["ArrowRight", KeyCode.ArrowRight],
+  ["NumpadAdd", KeyCode.NumpadAdd],
+  ["NumpadSubtract", KeyCode.NumpadSubtract],
+  ["NumpadMultiply", KeyCode.NumpadMultiply],
+  ["NumpadDivide", KeyCode.NumpadDivide],
+  ["NumpadSeparator", KeyCode.NumpadSeparator],
+  ["NumpadDecimal", KeyCode.NumpadDecimal],
+  ["PageUp", KeyCode.PageUp],
+  ["PageDown", KeyCode.PageDown],
+])("parseKey (key: %s)", (raw, code) => {
+  expect(parseKeyCode(raw)).toBe(code);
 });
 
-describe("isValidKeyCode()", () => {
-  test("true for valid key codes", () => {
-    expect(isValidKeyCode(KeyCode.LetterX)).toBeTruthy();
-  });
-
-  test("false for non key codes", () => {
-    expect(isValidKeyCode("cat")).toBeFalsy();
-  });
+test("parseKey f keys", () => {
+  for (let i = 1; i <= 12; i++) {
+    expect(parseKeyCode(`F${i}`)).toBe(`f${i}`);
+  }
 });
 
-describe("isModifier()", () => {
-  test.each([KeyCode.Meta, KeyCode.Control, KeyCode.Alt, KeyCode.Shift])(
-    "true for %s",
-    (keyCode) => {
-      expect(isModifier(keyCode)).toBe(true);
-    }
+test("parseKey digits", () => {
+  for (let i = 0; i <= 9; i++) {
+    expect(parseKeyCode(`Digit${i}`)).toBe(i.toString());
+  }
+});
+
+test("parseKey alphabet letters", () => {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+  for (const letter of alphabet) {
+    expect(parseKeyCode(`Key${letter}`)).toBe(letter.toLowerCase());
+  }
+});
+
+test("parseKey numpad digits", () => {
+  for (let i = 0; i <= 9; i++) {
+    expect(parseKeyCode(`Numpad${i}`)).toBe(`numpad${i}`);
+  }
+});
+
+test("isValidKeyCode true for valid key codes", () => {
+  expect(isValidKeyCode(KeyCode.LetterX)).toBeTruthy();
+});
+
+test("isValidKeyCode false for non key codes", () => {
+  expect(isValidKeyCode("cat")).toBeFalsy();
+});
+
+test.each([KeyCode.Meta, KeyCode.Control, KeyCode.Alt, KeyCode.Shift])(
+  "isModifier true for %s",
+  keyCode => {
+    expect(isModifier(keyCode)).toBe(true);
+  },
+);
+
+test("isModifier false for rando", () => {
+  expect(isModifier(KeyCode.LetterI)).toBe(false);
+});
+
+test.each([
+  KeyCode.Digit0,
+  KeyCode.Digit1,
+  KeyCode.Digit2,
+  KeyCode.Digit3,
+  KeyCode.Digit4,
+  KeyCode.Digit5,
+  KeyCode.Digit6,
+  KeyCode.Digit7,
+  KeyCode.Digit8,
+  KeyCode.Digit9,
+])("isNumber true for %s", key => {
+  expect(isNumber(key)).toBe(true);
+});
+
+test.each([KeyCode.Numpad3, KeyCode.Shift, KeyCode.Period])(
+  "isNumber is false for %s",
+  key => {
+    expect(isNumber(key)).toBe(false);
+  },
+);
+
+test.each([
+  KeyCode.LetterA,
+  KeyCode.LetterB,
+  KeyCode.LetterC,
+  KeyCode.LetterD,
+  KeyCode.LetterE,
+  KeyCode.LetterF,
+  KeyCode.LetterG,
+  KeyCode.LetterH,
+  KeyCode.LetterI,
+  KeyCode.LetterJ,
+  KeyCode.LetterK,
+  KeyCode.LetterL,
+  KeyCode.LetterM,
+  KeyCode.LetterN,
+  KeyCode.LetterO,
+  KeyCode.LetterP,
+  KeyCode.LetterQ,
+  KeyCode.LetterR,
+  KeyCode.LetterS,
+  KeyCode.LetterT,
+  KeyCode.LetterU,
+  KeyCode.LetterV,
+  KeyCode.LetterW,
+  KeyCode.LetterX,
+  KeyCode.LetterY,
+  KeyCode.LetterZ,
+])("isLetter true for %s", key => {
+  expect(isLetter(key)).toBe(true);
+});
+
+test.each([KeyCode.Numpad2, KeyCode.Digit5, KeyCode.Control])(
+  "isLetter false for %s",
+  key => {
+    expect(isLetter(key)).toBe(false);
+  },
+);
+
+test.each([
+  KeyCode.F1,
+  KeyCode.F2,
+  KeyCode.F3,
+  KeyCode.F4,
+  KeyCode.F5,
+  KeyCode.F6,
+  KeyCode.F7,
+  KeyCode.F8,
+  KeyCode.F9,
+  KeyCode.F10,
+  KeyCode.F11,
+  KeyCode.F12,
+])("isFn true for %s", key => {
+  expect(isFn(key)).toBe(true);
+});
+
+test.each([KeyCode.LetterA, KeyCode.Meta, KeyCode.Minus])(
+  "isFn false for",
+  key => {
+    expect(isFn(key)).toBe(false);
+  },
+);
+
+test.each([
+  KeyCode.Numpad0,
+  KeyCode.Numpad1,
+  KeyCode.Numpad2,
+  KeyCode.Numpad3,
+  KeyCode.Numpad4,
+  KeyCode.Numpad5,
+  KeyCode.Numpad6,
+  KeyCode.Numpad7,
+  KeyCode.Numpad8,
+  KeyCode.Numpad9,
+  KeyCode.Numpad0,
+  KeyCode.NumpadAdd,
+  KeyCode.NumpadDecimal,
+  KeyCode.NumpadDivide,
+  KeyCode.NumpadMultiply,
+  KeyCode.NumpadSeparator,
+  KeyCode.NumpadSubtract,
+])("isNumpadis true for %s", key => {
+  expect(isNumpad(key)).toBe(true);
+});
+
+test.each([KeyCode.LetterA, KeyCode.Digit5, KeyCode.Alt])(
+  "isNumpad is false for %s",
+  key => {
+    expect(isNumpad(key)).toBe(false);
+  },
+);
+
+test.each([
+  KeyCode.ArrowUp,
+  KeyCode.ArrowDown,
+  KeyCode.ArrowLeft,
+  KeyCode.ArrowRight,
+])("isArrowis true for %s", key => {
+  expect(isArrow(key)).toBe(true);
+});
+
+test.each([KeyCode.Numpad0, KeyCode.LetterA, KeyCode.NumpadSeparator])(
+  "isArrow is false for %s",
+  key => {
+    expect(isArrow(key)).toBe(false);
+  },
+);
+
+test.each([
+  KeyCode.Insert,
+  KeyCode.Escape,
+  KeyCode.Delete,
+  KeyCode.Backspace,
+  KeyCode.Tab,
+  KeyCode.CapsLock,
+  KeyCode.Enter,
+  KeyCode.Space,
+  KeyCode.PageDown,
+  KeyCode.PageUp,
+])("isActionis true for %s", key => {
+  expect(isAction(key)).toBe(true);
+});
+
+test.each([KeyCode.NumpadMultiply, KeyCode.LetterH, KeyCode.Meta])(
+  "isAction is false for %s",
+  key => {
+    expect(isAction(key)).toBe(false);
+  },
+);
+
+test("sortKeyCodes handles every expected key", () => {
+  const everyKey = Object.values(KeyCode);
+  expect(() => {
+    sortKeyCodes(everyKey);
+  }).not.toThrow();
+});
+
+test.each(["cat", "ctrl*1", ""])("parseKeyCodes throws %s", keyString => {
+  expect(() => parseKeyCode(keyString)).toThrow();
+});
+
+test("keyCodesToString works", () => {
+  expect(keyCodesToString([KeyCode.LetterH, KeyCode.Control])).toBe(
+    "control+h",
   );
-
-  test("false for rando", () => {
-    expect(isModifier(KeyCode.LetterI)).toBe(false);
-  });
-});
-
-describe("isNumber()", () => {
-  test.each([
-    KeyCode.Digit0,
-    KeyCode.Digit1,
-    KeyCode.Digit2,
-    KeyCode.Digit3,
-    KeyCode.Digit4,
-    KeyCode.Digit5,
-    KeyCode.Digit6,
-    KeyCode.Digit7,
-    KeyCode.Digit8,
-    KeyCode.Digit9,
-  ])("true for %s", (key) => {
-    expect(isNumber(key)).toBe(true);
-  });
-
-  test.each([KeyCode.Numpad3, KeyCode.Shift, KeyCode.Period])(
-    "is false for %s",
-    (key) => {
-      expect(isNumber(key)).toBe(false);
-    }
-  );
-});
-
-describe("isLetter()", () => {
-  test.each([
-    KeyCode.LetterA,
-    KeyCode.LetterB,
-    KeyCode.LetterC,
-    KeyCode.LetterD,
-    KeyCode.LetterE,
-    KeyCode.LetterF,
-    KeyCode.LetterG,
-    KeyCode.LetterH,
-    KeyCode.LetterI,
-    KeyCode.LetterJ,
-    KeyCode.LetterK,
-    KeyCode.LetterL,
-    KeyCode.LetterM,
-    KeyCode.LetterN,
-    KeyCode.LetterO,
-    KeyCode.LetterP,
-    KeyCode.LetterQ,
-    KeyCode.LetterR,
-    KeyCode.LetterS,
-    KeyCode.LetterT,
-    KeyCode.LetterU,
-    KeyCode.LetterV,
-    KeyCode.LetterW,
-    KeyCode.LetterX,
-    KeyCode.LetterY,
-    KeyCode.LetterZ,
-  ])("true for %s", (key) => {
-    expect(isLetter(key)).toBe(true);
-  });
-
-  test.each([KeyCode.Numpad2, KeyCode.Digit5, KeyCode.Control])(
-    "false for %s",
-    (key) => {
-      expect(isLetter(key)).toBe(false);
-    }
-  );
-});
-
-describe("isFn()", () => {
-  test.each([
-    KeyCode.F1,
-    KeyCode.F2,
-    KeyCode.F3,
-    KeyCode.F4,
-    KeyCode.F5,
-    KeyCode.F6,
-    KeyCode.F7,
-    KeyCode.F8,
-    KeyCode.F9,
-    KeyCode.F10,
-    KeyCode.F11,
-    KeyCode.F12,
-  ])("true for %s", (key) => {
-    expect(isFn(key)).toBe(true);
-  });
-
-  test.each([KeyCode.LetterA, KeyCode.Meta, KeyCode.Minus])(
-    "false for",
-    (key) => {
-      expect(isFn(key)).toBe(false);
-    }
-  );
-});
-
-describe("isNumpad()", () => {
-  test.each([
-    KeyCode.Numpad0,
-    KeyCode.Numpad1,
-    KeyCode.Numpad2,
-    KeyCode.Numpad3,
-    KeyCode.Numpad4,
-    KeyCode.Numpad5,
-    KeyCode.Numpad6,
-    KeyCode.Numpad7,
-    KeyCode.Numpad8,
-    KeyCode.Numpad9,
-    KeyCode.Numpad0,
-    KeyCode.NumpadAdd,
-    KeyCode.NumpadDecimal,
-    KeyCode.NumpadDivide,
-    KeyCode.NumpadMultiply,
-    KeyCode.NumpadSeparator,
-    KeyCode.NumpadSubtract,
-  ])("is true for %s", (key) => {
-    expect(isNumpad(key)).toBe(true);
-  });
-
-  test.each([KeyCode.LetterA, KeyCode.Digit5, KeyCode.Alt])(
-    "is false for %s",
-    (key) => {
-      expect(isNumpad(key)).toBe(false);
-    }
-  );
-});
-
-describe("isArrow()", () => {
-  test.each([
-    KeyCode.ArrowUp,
-    KeyCode.ArrowDown,
-    KeyCode.ArrowLeft,
-    KeyCode.ArrowRight,
-  ])("is true for %s", (key) => {
-    expect(isArrow(key)).toBe(true);
-  });
-
-  test.each([KeyCode.Numpad0, KeyCode.LetterA, KeyCode.NumpadSeparator])(
-    "is false for %s",
-    (key) => {
-      expect(isArrow(key)).toBe(false);
-    }
-  );
-});
-
-describe("isAction()", () => {
-  test.each([
-    KeyCode.Insert,
-    KeyCode.Escape,
-    KeyCode.Delete,
-    KeyCode.Backspace,
-    KeyCode.Tab,
-    KeyCode.CapsLock,
-    KeyCode.Enter,
-    KeyCode.Space,
-    KeyCode.PageDown,
-    KeyCode.PageUp,
-  ])("is true for %s", (key) => {
-    expect(isAction(key)).toBe(true);
-  });
-
-  test.each([KeyCode.NumpadMultiply, KeyCode.LetterH, KeyCode.Meta])(
-    "is false for %s",
-    (key) => {
-      expect(isAction(key)).toBe(false);
-    }
-  );
-});
-
-describe("sortKeyCodes()", () => {
-  test("handles every expected key", () => {
-    const everyKey = Object.values(KeyCode);
-    expect(() => {
-      sortKeyCodes(everyKey);
-    }).not.toThrow();
-  });
-});
-
-describe("parseKeyCodes()", () => {
-  test.each(["cat", "ctrl*1", ""])("throws %s", (keyString) => {
-    expect(() => parseKeyCode(keyString)).toThrow();
-  });
-});
-
-describe("keyCodesToString()", () => {
-  test("works", () => {
-    expect(keyCodesToString([KeyCode.LetterH, KeyCode.Control])).toBe(
-      "control+h"
-    );
-  });
 });
