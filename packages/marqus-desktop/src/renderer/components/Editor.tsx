c13e24dd1bc752e1c293276ce8a99e50720576b2
Eddie Abbondanzio <me@eddieabbondanz.io>
2023-04-18 23:58:38 -0400

    Reduce lag in shortcut system (#60)
    * Flush debounced functions when unmounting to prevent from losing changes
    
    * Fix useShortcuts to not need re-renders on key changes

--- packages/marqus-desktop/src/renderer/components/Editor.tsx
+++ packages/marqus-desktop/src/renderer/components/Editor.tsx
@@ -33,11 +33,13 @@ export function Editor(props: EditorProps): JSX.Element {
     store.on("editor.setContent", setContent);
     store.on("editor.save", save);
     store.on("editor.toggleView", toggleView);
+    store.on("editor.setModelViewState", setModelViewState);
 
     return () => {
       store.off("editor.setContent", setContent);
       store.off("editor.save", save);
       store.off("editor.toggleView", toggleView);
+      store.off("editor.setModelViewState", setModelViewState);
     };
   }, [store]);
 
@@ -86,6 +88,11 @@ const debouncedInvoker = debounce(
   NOTE_SAVE_INTERVAL_MS,
 ) as unknown as Ipc;
 
+// N.B. setContent and setViewState are stored in the parent Editor component
+// instead of Monaco because they may be invoked after the Monaco component has
+// been unmounted due to how we debounce / flush the debounced functions
+// on unmount.
+
 const setContent: Listener<"editor.setContent"> = async ({ value }, ctx) => {
   if (value == null) {
     return;
@@ -124,6 +131,22 @@ const setContent: Listener<"editor.setContent"> = async ({ value }, ctx) => {
   });
 
   await debouncedInvoker("notes.update", noteId, { content });
+};
+
+export const setModelViewState: Listener<"editor.setModelViewState"> = (
+  { value },
+  ctx,
+) => {
+  // TODO: Can we make listener parameters required?
+  if (value == null || value.noteId == null || value.modelViewState == null) {
+    return;
+  }
+
+  ctx.setCache({
+    modelViewStates: {
+      [value.noteId]: value.modelViewState,
+    },
+  });
 };
 
 const toggleView: Listener<"editor.toggleView"> = (_, ctx) => {
